a5aeb6a21dc57b067a9ce97c64f52ec0c03aadad
navyasharma9 <106896406+navyasharma9@users.noreply.github.com>
2022-06-19 18:00:39 +0530

    oopslabproject1

---
+++ Assignment/LabOops/src/com/gl/lab/Credential.java
@@ -0,0 +1,27 @@
+package com.gl.lab;
+import java.util.Random;
+public class Credential {
+	public static void main(String[] args) {
+		System.out.println(generatePassword(8));
+	}
+	public static char[] generatePassword(int length){
+		String capitalCaseLetter="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		String lowerCaseLetter="abcdefghijklmnopqrstuvwxyz";
+		String specialCharacter="!@#$";
+		String number="1234567890";
+		String combinedChars=capitalCaseLetter+lowerCaseLetter+specialCharacter+number;
+		Random random= new Random();
+		char[] password= new char[length];
+		password[0]=capitalCaseLetter.charAt(random.nextInt(capitalCaseLetter.length()));
+		password[1]=lowerCaseLetter.charAt(random.nextInt(lowerCaseLetter.length()));
+		password[2]=specialCharacter.charAt(random.nextInt(specialCharacter.length()));
+		password[3]=number.charAt(random.nextInt(number.length()));
+		for(int i=4;i<length;i++) {
+			password[i]=combinedChars.charAt(random.nextInt(combinedChars.length()));
+		}
+		return password;
+		
+		
+	}
+}
+
